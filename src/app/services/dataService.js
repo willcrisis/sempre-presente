ccd2b5abd69ab99d39df4150d6bd6aea01a14d74
Willian Krause <krause.willian@gmail.com>
2016-10-24 15:28:03 -0200

    Alunos ordenados por nome

--- src/app/services/dataService.js
+++ src/app/services/dataService.js
@@ -2,7 +2,7 @@ class DataService {
   constructor() {
     this.root = firebase.database().ref();
     this.professores = this.root.child('professores');
-    this.alunos = this.root.child('alunos');
+    this.alunos = this.root.child('alunos').orderByChild('nome');
     this.usuarios = this.root.child('usuarios');
     this.aulas = this.root.child('aulas');
   }
